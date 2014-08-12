32fb89ecd01e1b79b8046bbf0899c05ae800df2d
Kryzaal <kryzaal@gmail.com>
2014-08-12 22:20:46 +0200

    Mise en place du mailer, des pages contact et adhésion

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ var express = require("express");
 var compression = require('compression');
 var favicon = require('serve-favicon');
 var ejs = require('ejs');
+var mailer = require('nodemailer');
+var bodyParser = require('body-parser')
+
+var transporter = mailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'kryzaal',
+        pass: 'quantium7'
+    }
+});
 
 var app = express();
 
 app.use(favicon(__dirname + '/static/images/favicon.png'))
 .use(compression())
+.use(bodyParser.json())
+.use(bodyParser.urlencoded())
 .use(express.static(__dirname + "/static"))
 
 /** ROUTES **/
@@ -33,6 +45,33 @@ app.use(favicon(__dirname + '/static/images/favicon.png'))
     })
 })
 
+.post("/adherer", function(request, response){
+
+    var mailOptions = {
+        from: 'Action Française <kryzaal@gmail.com>',
+        to: request.body.email,
+        subject: 'Confirmation de votre inscription',
+        text: 'Inscription confirmée ' + request.body.prenom + ' ' + request.body.nom,
+        html: '<b>Inscription confirmée</b>' + request.body.prenom + ' ' + request.body.nom
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }
+
+        response.render('adherer.ejs', {
+            pageSubtitle: "Adhérer",
+            customStylesheets: ["adherer.css"],
+            toaster: {
+                color: error ? 'red' : 'green',
+                duration: 5,
+                html: error ? 'Inscription échouée' : 'Inscription réussie'
+            }
+        })
+    });
+})
+
 .get("/organigramme", function(request, response){
     response.render('organigramme.ejs', {
     	pageSubtitle: "Organigramme",
@@ -52,6 +91,31 @@ app.use(favicon(__dirname + '/static/images/favicon.png'))
     	pageSubtitle: "Contact",
     	customStylesheets: ["contact.css"]
     })
+})
+
+.post("/contact", function(request, response){
+    var mailOptions = {
+        from: request.body.email,
+        to: 'Action Française <kryzaal@gmail.com>',
+        subject: 'Message de ' + request.body.prenom + ' ' + request.body.nom,
+        text: request.body.message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }
+
+        response.render('contact.ejs', {
+            pageSubtitle: "Contact",
+            customStylesheets: ["contact.css"],
+            toaster: {
+                color: error ? 'red' : 'green',
+                duration: 5,
+                html: error ? 'Votre message n\'a pas été envoyé' : 'Merci pour votre message, nous y répondrons dans les meilleurs délais !'
+            }
+        })
+    });
 })
 
 .get("/dons", function(request, response){
