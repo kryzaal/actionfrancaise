9a7bc2a2772f8e11128fc80a6bb451a0d722835d
phdulac <pierrehenri.dr@gmail.com>
2014-08-14 17:15:14 +0200

    remplissage articles

--- server.js
+++ server.js
@@ -19,28 +19,28 @@ var auteur_blanchonnet = {
 
 var articles_demo = [
 	{id: 1, date: '23/05/2014', heure: '12:34', titre: 'CMRDS 2014',  sous_titre: 'Moment de camaraderie et de formation', 
-    resume: '<b>Vive le Roi</b>', complet: '', image: 'image_article2.jpg', auteur: auteur_blanchonnet},
+    resume: '<b>       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>', complet: '', image: 'image_article2.jpg', auteur: auteur_blanchonnet},
 
 	{id: 2, date: '24/05/2014', heure: '16:52', titre: 'Les identitaires rejoignent l\'AF', sous_titre: 'Le royalisme, seule conclusion possible au nationalisme.', 
-    resume: 'A mort la république !', complet: '', image: 'image_article1.jpg', auteur: auteur_blanchonnet},
+    resume: '<b>       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>', complet: '', image: 'image_article1.jpg', auteur: auteur_blanchonnet},
 
 	{id: 3, date: '23/05/2014', heure: '12:34', titre: 'CMRDS 2014', 
-	sous_titre: 'Moment de camaraderie et de formation', resume: '<b>Vive le Roi</b>', complet: '', image: 'image_article2.jpg', auteur: auteur_blanchonnet},
+	sous_titre: 'Moment de camaraderie et de formation', resume: '<b>       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>', complet: '', image: 'image_article3.jpg', auteur: auteur_blanchonnet},
 
 	{id: 4, date: '24/05/2014', heure: '16:52', titre: 'Les identitaires rejoignent l\'AF', 
-	sous_titre: 'Le royalisme, seule conclusion possible au nationalisme.', resume: 'A mort la république !', complet: '', image: 'image_article1.jpg', auteur: auteur_blanchonnet},
+	sous_titre: 'Le royalisme, seule conclusion possible au nationalisme.', resume: '<b> A mort la république !      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>', complet: '', image: 'image_article5.jpg', auteur: auteur_blanchonnet},
 
 	{id: 5, date: '23/05/2014', heure: '12:34', titre: 'CMRDS 2014', 
-	sous_titre: 'Moment de camaraderie et de formation', resume: '<b>Vive le Roi</b>', complet: '', image: 'image_article2.jpg', auteur: auteur_blanchonnet},
+	sous_titre: 'Moment de camaraderie et de formation', resume: '<b>       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>', complet: '', image: 'image_article2.jpg', auteur: auteur_blanchonnet},
 
 	{id: 6, date: '24/05/2014', heure: '16:52', titre: 'Les identitaires rejoignent l\'AF', 
-	sous_titre: 'Le royalisme, seule conclusion possible au nationalisme.', resume: 'A mort la république !', complet: '', image: 'image_article1.jpg', auteur: auteur_blanchonnet},
+	sous_titre: 'Le royalisme, seule conclusion possible au nationalisme.', resume: '<b>       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>', complet: '', image: 'image_article1.jpg', auteur: auteur_blanchonnet},
 
 	{id: 7, date: '23/05/2014', heure: '12:34', titre: 'CMRDS 2014', 
-	sous_titre: 'Moment de camaraderie et de formation', resume: '<b>Vive le Roi</b>', complet: '', image: 'image_article2.jpg', auteur: auteur_blanchonnet},
+	sous_titre: 'Moment de camaraderie et de formation', resume: '<b>       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>', complet: '', image: 'image_article3.jpg', auteur: auteur_blanchonnet},
 
 	{id: 8, date: '24/05/2014', heure: '16:52', titre: 'Les identitaires rejoignent l\'AF', 
-	sous_titre: 'Le royalisme, seule conclusion possible au nationalisme.', resume: 'A mort la république !', complet: '', image: 'image_article1.jpg', auteur: auteur_blanchonnet},
+	sous_titre: 'Le royalisme, seule conclusion possible au nationalisme.', resume: '<b>       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>', complet: '', image: 'image_article1.jpg', auteur: auteur_blanchonnet},
 ]; 
 
 var controllers = {
