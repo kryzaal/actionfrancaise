5b16ed61252f94f41725a796c736f2304ac32036
Kryzaal <kryzaal@gmail.com>
2014-08-19 20:59:14 +0200

    Thumbnails sur les vidéos

--- controllers/medias.js
+++ controllers/medias.js
@@ -1,8 +1,14 @@
+var fs = require("fs");
+
 function get(request, response) {
-	response.render('medias.ejs', {
-    	pageSubtitle: "Médias",
-    	customStylesheets: ["medias.css"]
-    })
+	var fileNames = fs.readdir(__dirname + "/../visuels", function(err, files) {
+        response.render('medias.ejs', {
+	    	pageSubtitle: "Médias",
+	    	customStylesheets: ["medias.css"],
+	    	visuels: files,
+	    	videos: ["1rC2plTGGBU", "1rC2plTGGBU", "5W-IIoqQHKs", "1jPOX72PPMw", "io3Yk1IGyaA", "e5WxM2qjpBo", "c7lxeFrHh9M", "Gf8oIxqaHRs", "1rC2plTGGBU"]
+    	})
+    });
 }
 
 exports.get = get;
