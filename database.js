d8f1632edc9371a73c2fc96bc2e497fbe31b7693
Kryzaal <kryzaal@gmail.com>
2014-09-04 23:37:30 +0200

    Base enfin fonctionnelle. Bonjour à Sync.

--- database.js
+++ database.js
@@ -1,16 +1,76 @@
 var sqlite3 = require('sqlite3').verbose();
 var fs = require('fs');
+var sync = require('sync');
 
 var dbPath = document_root + '/data/database.s3db';
 var backupPath = document_root + '/data/database.backup.s3db';
 var patch_dir = document_root + '/sql/patch/';
 
 var db = new sqlite3.Database(dbPath);
+
 process.on('exit', function(code) {
 	db.close();
 });
 
 var patchs = [];
-var sqlStack = [];
+
+function getSchemaVersion() {
+	var row = db.get.sync(db, "SELECT value FROM parameters WHERE (key == 'version')", {});
+
+	if(typeof(row) == 'undefined') return 0;
+	return parseInt(row.value);
+}
+
+function setSchemaVersion(version) {
+	return db.run.sync(db, "INSERT OR REPLACE INTO parameters (key, value) VALUES ($key, $value)", {'$key': 'version', '$value': version});
+}
+
+function upgradeSchema() {
+	if(patchs.length == 0) return;
+
+	var lowestPatch = patchs[0];
+	patchs.shift();
+	var schema_version = parseInt(lowestPatch.split('.')[0]);
+	var current_version = getSchemaVersion();
+
+	if(schema_version - current_version == 1) {
+		console.log('Upgrade vers la version du schema ' + schema_version);
+		executeSqlFile(patch_dir + lowestPatch);
+		setSchemaVersion(current_version + 1);
+		upgradeSchema();
+	}
+	else 
+		if(schema_version > current_version) throw "Impossible de passer de la version du schema " + version + " a la version " + schema_version + " : chainon manquant";
+}
+
+function executeSqlFile(file) {
+	var script = fs.readFileSync(file, {encoding: 'utf-8'});
+	var parts = script.replace('\n', ' ').replace('\r', '').replace('\t', '').split(';');
+
+	db.run.sync(db, 'BEGIN TRANSACTION', {});
+	parts.forEach(function(part) {
+		if(part.trim().length > 0)
+			db.run.sync(db, part, {});
+	});
+	db.run.sync(db, 'COMMIT', {});
+}
+
+sync(function() {
+	fs.createReadStream(dbPath).pipe(fs.createWriteStream(backupPath));
+
+	patchs = fs.readdirSync(patch_dir);
+	patchs.sort();
+
+	db.run.sync(db, "CREATE TABLE IF NOT EXISTS parameters (key TEXT PRIMARY KEY, value TEXT)", {});
+	db.run.sync(db, "INSERT OR IGNORE INTO parameters (key, value) VALUES ('version', '0')", {});
+
+	console.log("Version du schéma initiale : " + getSchemaVersion());
+
+	upgradeSchema();
+
+	console.log("Fin de l'initialisation de la base, version du schéma : " + getSchemaVersion());
+}, function(err){
+    if (err) throw err;
+});
 
 exports.db = db;
--- sql/patch/1.schema-initial.sql
+++ sql/patch/1.schema-initial.sql
@@ -1,5 +1,5 @@
-/*CREATE TABLE comptes (
-	nom_utilisateur TEXT PRIMARY_KEY,
+CREATE TABLE comptes (
+	nom_utilisateur TEXT PRIMARY KEY,
 	code_profil TEXT UNIQUE,
 	adresse_mail TEXT,
 	mot_passe TEXT NOT NULL,
@@ -7,23 +7,24 @@
 );
 
 CREATE TABLE droits_2_comptes (
-	nom_utilisateur TEXT PRIMARY_KEY,
-	code_droit TEXT PRIMARY_KEY
+	nom_utilisateur TEXT,
+	code_droit TEXT,
+	PRIMARY KEY(nom_utilisateur, code_droit)
 );
 
 CREATE TABLE droits (
-	code TEXT PRIMARY_KEY
+	code TEXT PRIMARY KEY
 );
 
 CREATE TABLE profil (
-	code TEXT PRIMARY_KEY,
+	code TEXT PRIMARY KEY,
 	nom TEXT NOT NULL,
 	titre TEXT,
 	biographie TEXT
 );
 
 CREATE TABLE texte (
-	code TEXT PRIMARY_KEY,
+	code TEXT PRIMARY KEY,
 	date_creation DATE DEFAULT CURRENT_DATE,
 	titre TEXT NOT NULL,
 	ss_titre TEXT
@@ -35,19 +36,19 @@ CREATE TABLE grand_texte (
 );
 
 CREATE TABLE article (
-	code_article TEXT PRIMARY_KEY,
+	code_article TEXT PRIMARY KEY,
 	code_texte TEXT UNIQUE
 );
 
 CREATE TABLE version_article (
-	code_article TEXT PRIMARY_KEY,
-	version INT PRIMARY_KEY AUTOINCREMENT,
+	code_article TEXT,
 	date_edition DATE DEFAULT CURRENT_DATE,
-	texte TEXT NOT NULL
+	texte TEXT NOT NULL,
+	PRIMARY KEY(code_article, date_edition)
 );
 
 CREATE TABLE entite (
-	code TEXT PRIMARY_KEY,
+	code TEXT PRIMARY KEY,
 	nom_court TEXT NOT NULL,
 	nom TEXT NOT NULL,
 	description TEXT,
@@ -55,17 +56,18 @@ CREATE TABLE entite (
 );
 
 CREATE TABLE membres_entites (
-	code_entite TEXT PRIMARY_KEY,
-	code_profil TEXT PRIMARY_KEY
+	code_entite TEXT,
+	code_profil TEXT,
+	PRIMARY KEY(code_entite, code_profil)
 );
 
 CREATE TABLE federation (
-	code_federation TEXT PRIMARY_KEY,
+	code_federation TEXT PRIMARY KEY,
 	code_entite TEXT UNIQUE
 );
 
 CREATE TABLE section (
-	code_section TEXT PRIMARY_KEY,
+	code_section TEXT PRIMARY KEY,
 	code_entite TEXT UNIQUE,
 	code_federation TEXT,
 	possede_section_afe BOOLEAN,
@@ -73,22 +75,23 @@ CREATE TABLE section (
 );
 
 CREATE TABLE action (
-	code TEXT PRIMARY_KEY,
+	code TEXT PRIMARY KEY,
 	code_entite_organisatrice TEXT,
 	date_debut DATE NOT NULL,
 	date_fin DATE,
 	nom TEXT NOT NULL,
-	description TEXT NOT NULL
+	description TEXT NOT NULL,
 	type TEXT NOT NULL
 );
 
 CREATE TABLE action_recurrente (
 	code_action TEXT UNIQUE,
-	code_groupe TEXT PRIMARY_KEY,
-	rang_action INTEGER PRIMARY_KEY,
-	nom_edition TEXT NOT NULL
+	code_groupe TEXT,
+	rang_action INTEGER ,
+	nom_edition TEXT NOT NULL,
+	PRIMARY KEY(code_groupe, rang_action)
 );
 
 CREATE TABLE type_action (
-	code_type TEXT PRIMARY_KEY
-);*/
+	code_type TEXT PRIMARY KEY
+);
