699c76de39f05e9eea90fddfe26c9ace321da48c
Kryzaal <kryzaal@gmail.com>
2014-09-18 01:38:13 +0200

    Sections

--- controllers/entites/sections.js
+++ controllers/entites/sections.js
@@ -1,15 +1,42 @@
 var model = require(document_root + '/models/section');
 var folder = require(document_root + '/lib/folder');
+var jsonLib = require(document_root + '/lib/json');
 
 var sectionsFolder = new folder.Folder(document_root + '/data/entites/sections');
 sectionsFolder.fallbackFileName = 'fallback.png';
 
+function existsOr404(request, response, key, callback) {
+	model.exists(request.params[key], function(err, exists) {
+		if(err) send500(response, true, err);
+		else if(!exists) send404(response, true);
+		else callback();
+	});
+} 
+
 function get(request, response) {
-	send501(response, false);
+	existsOr404(request, response, 'code', function() {
+		model.fetchOne(request.params.code, function(err, data) {
+			if(request.accepts('html')) {
+				response.render('entites_section.ejs', {
+			        pageSubtitle: data.nom,
+			        customStylesheets: ["entites_section"],
+			        section: data
+		    	});
+			} else {
+				var sender = new jsonLib.json(response);
+				sender.send(err, data);
+			}
+		});
+	});
 }
 
 function contact(request, response) {
-	send501(response, false);
+	existsOr404(request, response, 'code', function() {
+		model.fetchContact(request.params.code, function(err, data) {
+			var sender = new jsonLib.json(response);
+			sender.send(err, data);
+		});
+	});
 }
 
 function blason(request, response) {
@@ -17,15 +44,28 @@ function blason(request, response) {
 }
 
 function membres(request, response) {
-	send501(response, false);
+	existsOr404(request, response, 'code', function() {
+		model.fetchMembres(request.params.code, function(err, data) {
+			var sender = new jsonLib.json(response);
+			sender.send(err, data);
+		});
+	});
 }
 
 function federation(request, response) {
-	send501(response, false);
+	existsOr404(request, response, 'code', function() {
+		model.fetchFederation(request.params.code, function(err, data) {
+			var sender = new jsonLib.json(response);
+			sender.send(err, data);
+		});
+	});
 }
 
 function list(request, response) {
-	send501(response, false);
+	model.fetchAll(function(err, data) {
+		var sender = new jsonLib.json(response);
+		sender.send(err, data);
+	});
 }
 
 exports.get = get;
--- models/section.js
+++ models/section.js
@@ -1,7 +1,27 @@
-exports.fetchAll = function(callback) {
-	callback(false, data);
-};
+var dbHandler = require(document_root + '/database').handler;
 
-exports.exists = function(value, callback) {
-	callback(false, true);
+function fetchAll (callback) {
+
 }
+
+function fetchOne(code, callback) {
+}
+
+function fetchMembres(code, callback) {
+}
+
+function fetchFederation(code, callback) {
+}
+
+function fetchContact(code, callback) {
+}
+
+function exists(code, callback) {
+}
+
+exports.fetchAll = fetchAll;
+exports.fetchOne = fetchOne;
+exports.fetchMembres = fetchMembres;
+exports.fetchFederation = fetchFederation;
+exports.fetchContact = fetchContact;
+exports.exists = exists;
